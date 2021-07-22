f39233e8e17c65748c02443763e7d26a0a08dc4c
sahilwk <86531138+sahilwk@users.noreply.github.com>
2021-07-22 11:39:49 +0530

    very simple generic application

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s=new sample<double>();//uses double
+            s.name=90.3;
+            System.Console.WriteLine(s.func());
+
+            var s1=new sample<string>(); //uses string
+            s1.name="sahil";
+          
+            System.Console.WriteLine(s1.func());
+
+
+            var s2=new sample<object>();  //can have both string and double but considers the latest assigned value for computing
+             
+            s2.name=98.7;
+            s2.name="sahilassasa";
+            System.Console.WriteLine(s2.func());
+        }
+    }
+}
---
+++ sample.cs
@@ -0,0 +1,11 @@
+using System;
+namespace generics{
+    public class sample<T>{
+        
+        public T name;
+
+        public T func(){
+            return name;
+        }
+    }
+}
